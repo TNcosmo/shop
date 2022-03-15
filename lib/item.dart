91f7eed25c981d23de6fca9f4341fc610bce9145
unknown <HIF@ThinkBook08.htl-stp.if>
2022-03-15 11:18:30 +0100

    Initial commit

---
+++ lib/item.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+class Fach{
+  final String name;
+  final double price;
+  final File image;
+  final bool fave;
+
+  Fach(this.name, this.price, this.image, this.fave);
+
+  @override
+  String toString() {
+    return super.toString();
+  }
+
+}
